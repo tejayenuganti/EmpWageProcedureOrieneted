19eab890e972d5e79418fe9cd465801abbe38d62
Teja <tejasriyenuganti@gmail.com>
2020-07-15 21:45:32 +0530

    Adding attendence

---
+++ Attendence.java
@@ -0,0 +1,19 @@
+public class Attendence
+{
+	public static void main (String[] args)
+	{
+		int Attendence=1;
+		double empFind=Math.floor(Math.random() * 10)%2;
+		if(empFind == Attendence)
+		{
+			System.out.println("Employee is present");
+		}
+		else
+		{
+			System.out.println("Employee is absent");
+		}
+
+	}
+
+
+}
