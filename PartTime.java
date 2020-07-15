9d210839e04722775d4924290395c330a8d29db9
Teja <tejasriyenuganti@gmail.com>
2020-07-15 22:31:39 +0530

    Added , Parttime

---
+++ PartTime.java
@@ -0,0 +1,27 @@
+public class PartTime
+{
+	public static void main (String[] args)
+	{
+		int fullTime=1;
+		int partTime=2;
+		int wagePerHour=20;
+		int empWage=0;
+		int empHrs=0;
+
+		double empFind=Math.floor(Math.random() * 10)%3;
+		if(empFind == fullTime)
+		{
+			empHrs=8;
+		}
+		else if(empFind == partTime)
+		{
+			empHrs=4;
+		}
+		else
+		{
+			empHrs=0;
+		}
+		empWage = empHrs * wagePerHour;
+		System.out.println("Employee wage per day is" +empWage);
+	}
+}
